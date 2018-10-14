39787b9b2b207a94b97660138c006e5e8a6390eb
Mézes Ádám <mezes.adam@webdesign.co.hu>
2018-10-14 14:37:27 +0200

    Projekt alap fájljainak beállítása (JPA, DevTools, H2, Lombok, Web, és Security) application.properties beállítása (adatbázis neve mozi) import.sql létrehozása

---
+++ src/main/java/hu/elte/AlkfejlMozi/AlkfejlMoziApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.AlkfejlMozi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkfejlMoziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkfejlMoziApplication.class, args);
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,8 @@
+/**
+ * Az alap SQL importok
+ *
+ * Author:  Mézes Ádám
+ * Created: 2018.10.14.
+ */
+
+// TO DO
