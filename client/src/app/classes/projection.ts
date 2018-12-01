337a481f44d219f7c750ec4b99f910159b5191cb
Mézes Ádám <mezesadam1997@gmail.com>
2018-12-01 09:50:57 +0100

    Felesleges fájlok törlése Ütközések javítása

--- client/src/app/classes/projection.ts
+++ client/src/app/classes/projection.ts
@@ -6,12 +6,7 @@ export class Projection {
     public id: number;
     public price: number;
     public is3d: boolean;
-<<<<<<< HEAD
     public time: Date;
-=======
-    public startingTime: Date;
->>>>>>> fcb68fab39212a02c1e92a01d06113b26fc39f15
-    
     public room: Room;
     public movie: Movie;
     public tickets: Ticket[];
--- client/src/app/reservations/reservations.component.ts
+++ client/src/app/reservations/reservations.component.ts
@@ -1,11 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { Ticket } from '../classes/ticket';
 import { TicketService } from '../services/ticket.service';
-=======
-import { Reservation } from '../classes/reservation';
-import { ReservationService } from '../services/reservation.service';
->>>>>>> fcb68fab39212a02c1e92a01d06113b26fc39f15
 
 @Component({
   selector: 'app-reservations',
@@ -14,23 +9,13 @@ import { ReservationService } from '../services/reservation.service';
 })
 export class ReservationsComponent implements OnInit {
 
-<<<<<<< HEAD
-  private _reservations: Ticket[];
+  private _tickets: Ticket[];
   private displayedColumns = ['title', 'date', 'seat', 'row'];
 
-  constructor(private _reservationService: TicketService) { }
+  constructor(private _ticketService: TicketService) { }
 
   ngOnInit() {
-    //this._reservations = this._reservationService.getReservations();
-=======
-  private _reservations: Reservation[];
-  private displayedColumns = ['title', 'date', 'seat', 'row'];
-
-  constructor(private _reservationService: ReservationService) { }
-
-  ngOnInit() {
-    this._reservations = this._reservationService.getReservations();
->>>>>>> fcb68fab39212a02c1e92a01d06113b26fc39f15
+    //this._tickets = this._ticketService.getTickets();
   }
 
 }
